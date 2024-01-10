7438507289fa31f5408b8965a6560bb29fa430fe
Christian <bournecreative@gmail.com>
2024-01-10 16:01:05 -0600

    restructured how data is saved. Each row is a object with a value of of an array of properties. Properties are key value pairs. Each item or row of data has a properties named according to the CSV header column names

--- parseCSV.js
+++ parseCSV.js
@@ -6,17 +6,26 @@ const jsonObj = [];
 const headers = arr[0].split(",");
 for (let i = 0; i < arr.length; i++) {
   const row = arr[i].split(",");
-  let obj = {};
-  for (let j = 0; j < row.length; j++) {
-    obj[headers[j].trim()] = row[j].trim();
-  }
-  jsonObj.push(obj);
+  const objRow = [];
+  row.map((i, index) => {
+    const obj = { [`${headers[index]}`]: i };
+    objRow.push(obj);
+  });
+
+  jsonObj.push(objRow);
+  const JSONObj = JSON.stringify(jsonObj);
+
+  // for (let j = 0; j < row.length; j++) {
+  //   obj[headers[j].trim()] = row[j].trim();
+  // }
+  // jsonObj.push(obj);
 }
+// console.log(jsonObj);
+// console.log(jsonObj);
+// const sortedByYear = jsonObj.sort(
+//   (a, b) => parseInt(a.TIME) - parseInt(b.TIME)
+// );
 
-const sortedByYear = jsonObj.sort(
-  (a, b) => parseInt(a.TIME) - parseInt(b.TIME)
-);
-
-sortedByYear.map((item) => {
-  console.log(`Country:${item.LOCATION} Year:${item.TIME} Debt:${item.Value}`);
-});
+// sortedByYear.map((item) => {
+//   console.log(`Country:${item.LOCATION} Year:${item.TIME} Debt:${item.Value}`);
+// });
