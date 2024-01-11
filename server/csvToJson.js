183e08ed4265c06b18383ed60711059a50d712cc
Christian <bournecreative@gmail.com>
2024-01-10 22:03:03 -0600

    refactor functionality into a function in a seperate module. Right now the goal is simply to input a CSV, parse the data in JSON and make this accessible as a function so it can be called from a seperate module

---
+++ server/csvToJson.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+csvFile = fs.readFileSync("./houseHoldDebt.csv");
+
+function csvToJSON() {
+  const jsonObj = [];
+  const arr = csvFile.toString().replace(/\r/g, "").split("\n");
+  const headers = arr[0].split(",");
+
+  for (let i = 1; i < arr.length; i++) {
+    const row = arr[i].split(",");
+    const objRow = [];
+    row.map((i, index) => {
+      const obj = { [`${headers[index]}`]: i };
+      objRow.push(obj);
+    });
+    jsonObj.push(objRow);
+  }
+  return jsonObj;
+}
+
+module.exports.csvToJSON = csvToJSON;
+// for (let j = 0; j < row.length; j++) {
+//   obj[headers[j].trim()] = row[j].trim();
+// }
+// jsonObj.push(obj);
+
+// console.log(jsonObj);
+// console.log(jsonObj);
+// const sortedByYear = jsonObj.sort(
+//   (a, b) => parseInt(a.TIME) - parseInt(b.TIME)
+// );
+
+// sortedByYear.map((item) => {
+//   console.log(`Country:${item.LOCATION} Year:${item.TIME} Debt:${item.Value}`);
+// });
