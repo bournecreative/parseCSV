91da32a3ab4efbf64d515434d937366c54c8c511
Christian <bournecreative@gmail.com>
2024-01-12 21:58:54 -0600

    re-organize how CSV data is pushed into array

--- server/csvToJson.js
+++ server/csvToJson.js
@@ -4,14 +4,13 @@ csvFile = fs.readFileSync("./houseHoldDebt.csv");
 function csvToJSON() {
   const jsonObj = [];
   const arr = csvFile.toString().replace(/\r/g, "").split("\n");
-  const headers = arr[0].split(",");
-
+  const headers = arr[0].split(",").map((i) => i.toLowerCase());
+  jsonObj.push(headers);
   for (let i = 1; i < arr.length; i++) {
     const row = arr[i].split(",");
-    const objRow = [];
+    const objRow = {};
     row.map((i, index) => {
-      const obj = { [`${headers[index]}`]: i };
-      objRow.push(obj);
+      objRow[`${headers[index]}`] = i.toLowerCase();
     });
     jsonObj.push(objRow);
   }
