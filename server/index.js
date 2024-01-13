97287c54a22c673ce71d6b245521e2f23dded124
Christian <bournecreative@gmail.com>
2024-01-12 21:56:51 -0600

    set cors for server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const http = require("http");
 const getCSV = require("./csvToJson");
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "text/javascript",
+  });
   if (req.url === "/") {
     const test = getCSV.csvToJSON();
     res.write(JSON.stringify(test));
